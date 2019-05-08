791daab683201cd8ab1858e94ee8ca537021311a
Kezia Suhendra <keziasuhendra44@gmail.com>
2019-05-09 01:13:34 +0700

    add spinner for waiting data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Home from './Home';
+import Loading from './Loading';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -14,6 +15,7 @@ ReactDOM.render(
     <div>
     <Route exact path='/' component={Home} />
     <Route path='/app' component={App} />
+    <Route path='/loading' component={Loading} />
     </div>
 </Router>, document.getElementById('root'));
 registerServiceWorker();
